db2c0fb715d1aa4340dde20fb4a915448aec2652
남기성 <kaidoma@gmail.com>
2018-08-08 00:22:52 +0900

    DoubleZero Telegram Bot Ver. 1.1  - Load orderbook instead of ticker  - Able to set target difference and target volume

--- bot.py
+++ bot.py
@@ -15,9 +15,12 @@ chat_id = ''
 print("Creating DoubleZero's Telegram Bot...")
 bot = telegram.Bot(token=my_token)
 
-# Target Values
+# Parameters
 
-target_diff = 0.5  # 이 값 이상 차이날 경우 알림
+target_diff = 0.4  # 이 값 이상 차이날 경우 알림 (단위 : %)
+target_volume = 100000  # 이 값 이상 거래할 수 있을때만 알림 (단위 : KRW)
+time_interval = 10  # 반복 주기 (단위 : 초)
+coins = ["BTC", "ETH", "XRP", "BCH", "EOS"]  # 관찰할 코인 리스트
 
 # Define Functions
 
@@ -28,91 +31,98 @@ def telegram_send(text):
 
 
 def diff(num1, num2):
-    if num1 > num2:
-        return round((num1 - num2) / num2 * 100, 2)
-    else:
-        return round((num2 - num1) / num1 * 100, 2)
+    return round((num1 - num2) / num2 * 100, 2)
 
 
 def run_bot():
-    print("Loading Bithumb Price")
-    api_url_bithumb = "https://api.bithumb.com/public/ticker/all"
-    resp = requests.get(api_url_bithumb)
-    result = json.loads(resp.text)
-    bithumb_BTC = int(float(result["data"]["BTC"]["closing_price"]))
-    bithumb_ETH = int(float(result["data"]["ETH"]["closing_price"]))
-    bithumb_XRP = int(float(result["data"]["XRP"]["closing_price"]))
-    bithumb_BCH = int(float(result["data"]["BCH"]["closing_price"]))
-    bithumb_EOS = int(float(result["data"]["EOS"]["closing_price"]))
+    # Load Bithumb Orderbook
+    print("Loading Bithumb Orderbook")
+    api_url_bithumb = "https://api.bithumb.com/public/orderbook/all"
+    bithumb_orderbook_response = requests.get(api_url_bithumb)
+    bithumb_orderbook_result = json.loads(bithumb_orderbook_response.text)
 
-    print("Loading Upbit Price")
-    api_url_upbit_btc = "https://api.upbit.com/v1/ticker?markets=KRW-BTC"
-    resp = requests.get(api_url_upbit_btc)
-    result = json.loads(resp.text)
-    upbit_BTC = int(float(result[0]["trade_price"]))
+    # Get First Bid Ask Price and Quantity at Bithumb
+    bithumb_bid = []
+    bithumb_bid.append(bithumb_orderbook_result["data"]["BTC"]["bids"][0])
+    bithumb_bid.append(bithumb_orderbook_result["data"]["ETH"]["bids"][0])
+    bithumb_bid.append(bithumb_orderbook_result["data"]["XRP"]["bids"][0])
+    bithumb_bid.append(bithumb_orderbook_result["data"]["BCH"]["bids"][0])
+    bithumb_bid.append(bithumb_orderbook_result["data"]["EOS"]["bids"][0])
 
-    api_url_upbit_eth = "https://api.upbit.com/v1/ticker?markets=KRW-ETH"
-    resp = requests.get(api_url_upbit_eth)
-    result = json.loads(resp.text)
-    upbit_ETH = int(float(result[0]["trade_price"]))
+    bithumb_ask = []
+    bithumb_ask.append(bithumb_orderbook_result["data"]["BTC"]["asks"][0])
+    bithumb_ask.append(bithumb_orderbook_result["data"]["ETH"]["asks"][0])
+    bithumb_ask.append(bithumb_orderbook_result["data"]["XRP"]["asks"][0])
+    bithumb_ask.append(bithumb_orderbook_result["data"]["BCH"]["asks"][0])
+    bithumb_ask.append(bithumb_orderbook_result["data"]["EOS"]["asks"][0])
 
-    api_url_upbit_xrp = "https://api.upbit.com/v1/ticker?markets=KRW-XRP"
-    resp = requests.get(api_url_upbit_xrp)
-    result = json.loads(resp.text)
-    upbit_XRP = int(float(result[0]["trade_price"]))
+    # Load Upbit Orderbook
+    print("Loading Upbit Orderbook")
+    api_url_upbit_BTC = "https://api.upbit.com/v1/orderbook?markets=KRW-BTC"
+    upbit_orderbook_BTC_response = requests.get(api_url_upbit_BTC)
+    upbit_orderbook_BTC_result = json.loads(upbit_orderbook_BTC_response.text)
 
-    api_url_upbit_bch = "https://api.upbit.com/v1/ticker?markets=KRW-BCH"
-    resp = requests.get(api_url_upbit_bch)
-    result = json.loads(resp.text)
-    upbit_BCH = int(float(result[0]["trade_price"]))
+    api_url_upbit_ETH = "https://api.upbit.com/v1/orderbook?markets=KRW-ETH"
+    upbit_orderbook_ETH_response = requests.get(api_url_upbit_ETH)
+    upbit_orderbook_ETH_result = json.loads(upbit_orderbook_ETH_response.text)
 
-    api_url_upbit_eos = "https://api.upbit.com/v1/ticker?markets=KRW-EOS"
-    resp = requests.get(api_url_upbit_eos)
-    result = json.loads(resp.text)
-    upbit_EOS = int(float(result[0]["trade_price"]))
+    api_url_upbit_XRP = "https://api.upbit.com/v1/orderbook?markets=KRW-XRP"
+    upbit_orderbook_XRP_response = requests.get(api_url_upbit_XRP)
+    upbit_orderbook_XRP_result = json.loads(upbit_orderbook_XRP_response.text)
 
-    difference_BTC = diff(bithumb_BTC, upbit_BTC)
-    print(difference_BTC)
-    difference_ETH = diff(bithumb_ETH, upbit_ETH)
-    print(difference_ETH)
-    difference_XRP = diff(bithumb_XRP, upbit_XRP)
-    print(difference_XRP)
-    difference_BCH = diff(bithumb_BCH, upbit_BCH)
-    print(difference_BCH)
-    difference_EOS = diff(bithumb_EOS, upbit_EOS)
-    print(difference_EOS)
+    api_url_upbit_BCH = "https://api.upbit.com/v1/orderbook?markets=KRW-BCH"
+    upbit_orderbook_BCH_response = requests.get(api_url_upbit_BCH)
+    upbit_orderbook_BCH_result = json.loads(upbit_orderbook_BCH_response.text)
 
-    text_BTC = "빗썸 BTC 가격 : %s \n업비트 BTC 가격 : %s \n차이 : %s%%" % (
-        bithumb_BTC, upbit_BTC, difference_BTC)
-    text_ETH = "빗썸 ETH 가격 : %s \n업비트 ETH 가격 : %s \n차이 : %s%%" % (
-        bithumb_ETH, upbit_ETH, difference_ETH)
-    text_XRP = "빗썸 XRP 가격 : %s \n업비트 XRP 가격 : %s \n차이 : %s%%" % (
-        bithumb_XRP, upbit_XRP, difference_XRP)
-    text_BCH = "빗썸 BCH 가격 : %s \n업비트 BCH 가격 : %s \n차이 : %s%%" % (
-        bithumb_BCH, upbit_BCH, difference_BCH)
-    text_EOS = "빗썸 EOS 가격 : %s \n업비트 EOS 가격 : %s \n차이 : %s%%" % (
-        bithumb_EOS, upbit_EOS, difference_EOS)
+    api_url_upbit_EOS = "https://api.upbit.com/v1/orderbook?markets=KRW-EOS"
+    upbit_orderbook_EOS_response = requests.get(api_url_upbit_EOS)
+    upbit_orderbook_EOS_result = json.loads(upbit_orderbook_EOS_response.text)
 
-    if difference_BTC > target_diff:
-        telegram_send(text_BTC)
+    # Get First Bid Ask Price and Quantity at Upbit
+    upbit_orderbook = []
+    upbit_orderbook.append(upbit_orderbook_BTC_result[0]["orderbook_units"][0])
+    upbit_orderbook.append(upbit_orderbook_ETH_result[0]["orderbook_units"][0])
+    upbit_orderbook.append(upbit_orderbook_XRP_result[0]["orderbook_units"][0])
+    upbit_orderbook.append(upbit_orderbook_BCH_result[0]["orderbook_units"][0])
+    upbit_orderbook.append(upbit_orderbook_EOS_result[0]["orderbook_units"][0])
 
-    if difference_ETH > target_diff:
-        telegram_send(text_ETH)
+    # Arbitrage Decision between Bithumb and Upbit
+    # Step 1. Bithumb Ask vs. Upbit Bid (Buy at Bithumb and Sell at Upbit)
+    for i in range(len(bithumb_ask)):
+        bithumb_ask_price = int(bithumb_ask[i]["price"])
+        bithumb_ask_quantity = float(bithumb_ask[i]["quantity"])
+        upbit_bid_price = int(upbit_orderbook[i]["bid_price"])
+        upbit_bid_quantity = float(upbit_orderbook[i]["bid_size"])
+        available_size = int(round(min(bithumb_ask_quantity,
+                                       upbit_bid_quantity) * bithumb_ask_price))
+        difference = diff(upbit_bid_price, bithumb_ask_price)
 
-    if difference_XRP > target_diff:
-        telegram_send(text_XRP)
+        if difference > target_diff and available_size > target_volume:
+            text = "[%s] 기회 발생\n빗썸 매수 : %s / %s %s\n업비트 매도 : %s / %s %s\n차이 : %s%%\n거래 가능 규모 : %s원" % (
+                coins[i], bithumb_ask_price, bithumb_ask_quantity, coins[i], upbit_bid_price, upbit_bid_quantity, coins[i], difference, available_size)
+            telegram_send(text)
 
-    if difference_BCH > target_diff:
-        telegram_send(text_BCH)
+    # Step 1. Bithumb Bid vs. Upbit Ask (Buy at Upbit and Sell at Bithumb)
+    for i in range(len(bithumb_bid)):
+        bithumb_bid_price = int(bithumb_bid[i]["price"])
+        bithumb_bid_quantity = float(bithumb_bid[i]["quantity"])
+        upbit_ask_price = int(upbit_orderbook[i]["ask_price"])
+        upbit_ask_quantity = float(upbit_orderbook[i]["ask_size"])
+        available_size = int(round(min(bithumb_bid_quantity,
+                                       upbit_ask_quantity) * upbit_ask_price))
+        difference = diff(bithumb_bid_price, upbit_ask_price)
 
-    if difference_EOS > target_diff:
-        telegram_send(text_EOS)
+        if difference > target_diff and available_size > target_volume:
+            text = "[%s] 기회 발생\n업비트 매수 : %s / %s %s\n빗썸 매도 : %s / %s %s\n차이 : %s%%\n거래 가능 규모 : %s원" % (
+                coins[i], upbit_ask_price, upbit_ask_quantity, coins[i], bithumb_bid_price, bithumb_bid_quantity, coins[i], difference, available_size)
+            telegram_send(text)
 
 
 # Running Part
 
+
 while True:
     run_bot()
-    time.sleep(10)
+    time.sleep(time_interval)
 
 print("Done.")
